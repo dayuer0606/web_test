e0f2c8231f3b58c121cde34a0bf0ac6e81a027b9
v_wenyuxie <v_wenyuxie@tencent.com>
2021-06-01 16:44:39 +0800

    add test

---
+++ js/java_script.js
@@ -0,0 +1,8 @@
+$("#button1").click(function (){
+        $("body").css("background-color","green");
+        // alert("123");
+})
+$("#button2").click(function (){
+        $("body").css("background-color","red");
+        // alert("123");
+})
